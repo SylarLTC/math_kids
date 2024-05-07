899deae82663c7169fb95bf95c7e8f174dcadb3b
Adilzhan <abalgabekov@gmail.com>
2024-05-07 19:30:07 +0100

    fixed bugs

--- front/src/components/Division.js
+++ front/src/components/Division.js
@@ -1,14 +1,10 @@
 import React from "react";
 import { dbRandomNumbersDivision } from "../db/db";
 import { DivisionItem } from "./DivisionItem";
-// import { TimeTable } from "./TimeTable";
 
 export const Division = ({ totalCorrects, setTotalCorrects }) => {
   return (
     <div className="flex relative">
-      {/* <div className="absolute right-0 top-0 z-10">
-        <TimeTable />
-      </div> */}
       <div className="flex flex-wrap justify-around mt-10 gap-4">
         {dbRandomNumbersDivision.map((item) => {
           return (
